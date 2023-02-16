955781bc867678a882e52c978c5f3eacb244fcfa
Dnyaneshwar ghodse <dnyaneshwarghodse@analytic-edge.com>
2023-02-16 10:47:37 +0530

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='footer'>
+        <div className='contact'>
+          <h4>Contact</h4>
+          <div>
+            <p>Satish More</p>
+            <h4>90909090990</h4>
+          </div>
+        </div>
         <div className="address">
-            Vasarni 
+          <h4>Address</h4>
+             <p>Mujampeth/Dhanegaon, Navghat Road, New Nanded, 431603</p>
         </div>
     </div>
   )
